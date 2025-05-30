8acf5f31ad8df09b9cebd42e90d7e4292420b9a6
vaibhavityagi <vaibhavicodes@gmail.com>
2025-05-31 00:44:36 +0530

    favorites and search changes

--- WEB-INF/classes/com/vee/bingeBuddy/FavoriteServlet.java
+++ WEB-INF/classes/com/vee/bingeBuddy/FavoriteServlet.java
@@ -2,9 +2,6 @@ import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import com.vee.bingeBuddy.DatabaseConnection;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.sql.*;
 
 public class FavoriteServlet extends HttpServlet {
